36795723ca558a56809c90771e7d697964e9eb82
Juliette Sofia Lizarazo Ardila <jlizarazoa@unal.edu.co>
2022-10-05 17:56:13 -0500

    Late late changes, ready now

--- sia_calificaciones/resolver.js
+++ sia_calificaciones/resolver.js
@@ -68,53 +68,17 @@ export const root = {
     },
 
     createGrades: (arg) => {
-        let query = `
-        {
-            asignatura(codigo_asignatura: ${arg.id_asignature}) {
-                codigo_asignatura
-            }
-        }`
-        return fetch(`${API_URL_BUSCADOR_CURSOS}`, {
+        return fetch(`${API_URL}/api/grades/create`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 "Accept": "application/json",
             },
-            body: JSON.stringify({
-                query,
-            })
+            body: JSON.stringify(arg)
         })
             .then(response => response.json())
             .then(data => {
-                // TODO: Must check if the asignature exists, when it does not exist, it returns a [null] value
-                return fetch(`${API_URL_INSCRIPCIONES}/inscripcion/${arg.id_asignature}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    // TODO: Should have a body with the request to be sent
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        // TODO: [data] is a Json with .data and .valueOFQuery properties
-                        let result = _.find(data, {'documento_estudiante': arg.id_students})
-                        if (result)
-                        {
-                            return fetch(`${API_URL}/api/grades/create`, {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                "Accept": "application/json",
-                            },
-                            body: JSON.stringify(arg)
-                        })
-                            .then(response => response.json())
-                            .then(data => {
-                                return data
-                            })
-                        }
-                        
-                    })
+                return data
             })
     },
 
