aea442514955c1f13fe51dc6dc2f8ee04ee63317
Juliette Sofia Lizarazo Ardila <jlizarazoa@unal.edu.co>
2022-10-17 23:35:50 -0500

    Updated queries und updated model from asignatures to courses.

--- sia_calificaciones/resolver.js
+++ sia_calificaciones/resolver.js
@@ -1,3 +1,4 @@
+import { query, response } from "express"
 import fetch from "node-fetch"
 import { API_URL } from "./index.js"
 
@@ -11,30 +12,73 @@ import { API_URL } from "./index.js"
  **/
 
 export const root = {
-    listGrades: (arg) => {
-        let asignatura = arg.id !== null ? `?id=${arg.id}` : `?id_asignature=${arg.asignature}`
-
-        return fetch(`${API_URL}/api/grades/${asignatura}`)
+    listAll: (arg) => {
+        return fetch(`${API_URL}/api/all/`)
             .then(response => response.json())
             .then(data => {
-                return data
+                let jsonHistory = data["Histories"];
+                let jsonCourses = data["Courses"];
+                let jsonGrades = data["Grades"];
+
+                const jsonFull = {
+                    history: jsonHistory,
+                    courses: jsonCourses,
+                    grades: jsonGrades
+                }
+                return jsonFull
             })
+    },
+
+    listGrades: (arg) => {
+        if (arg.id == null && arg.course == null) {
+            return fetch(`${API_URL}/api/grades/`)
+                .then(response => response.json())
+                .then(data => {
+                    return data
+                })
+        }
+        else {
+            let asignatura = arg.id !== null ? `?id=${arg.id}` : `?id_course=${arg.asignature}`
+            return fetch(`${API_URL}/api/grades/${asignatura}`)
+                .then(response => response.json())
+                .then(data => {
+                    
+                    return data
+                })
+        }
     },
     
 
-    listAsignatures: (arg) => {
-        let termn = arg.id !== null ? `?id=${arg.id}` : `?id_term=${arg.termn}`
-        
-        return fetch(`${API_URL}/api/asignatures/?id=${termn}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                return data
-            })
+    listCourse: (arg) => {
+        if (arg.id == null && arg.termn === null) {
+            return fetch(`${API_URL}/api/courses/`)
+                .then(response => response.json())
+                .then(data => {
+                    
+                    return data
+                })
+        }
+        else {
+            let termn = arg.id !== null ? `?id=${arg.id}` : `?term=${arg.termn}`
+
+            return fetch(`${API_URL}/api/courses/${termn}`)
+                .then(response => response.json())
+                .then(data => {
+                    return data
+                })
+        }
     },
 
     listHistory: (arg) => {
-        let program = arg.id !== null ? `?id=${arg.id}` : `?id_program=${arg.program}`
+        if (arg.id == null && arg.program === null) {
+            return fetch(`${API_URL}/api/history/`)
+                .then(response => response.json())
+                .then(data => {
+                    return data
+                })
+        }
+        else {
+            let program = arg.id !== null ? `?id=${arg.id}` : `?id_program=${arg.program}`
 
         return fetch(`${API_URL}/api/history/?id=${program}`)
             .then(response => response.json())
@@ -42,9 +86,13 @@ export const root = {
                 console.log(data)
                 return data
             })
+        }
     },
 
     createGrades: (arg) => {
+        let gradesdata = arg.grades;
+        var dict = JSON.parse(gradesdata.replace(/'/g,'"'));
+        arg.grades = dict;
         return fetch(`${API_URL}/api/grades/create`, {
             method: 'POST',
             headers: {
@@ -61,6 +109,9 @@ export const root = {
     },
 
     updateGrades: async (arg) => {
+        let gradesdata = arg.grades;
+        var dict = JSON.parse(gradesdata.replace(/'/g,'"'));
+        arg.grades = dict;
         return fetch(`${API_URL}/api/grades/update/${arg.id}`, {
             method: 'PUT',
             headers: {
@@ -82,6 +133,18 @@ export const root = {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
+            },
+        })
+            .then(data => {
+                return data
+            })
+    },
+
+    updateCourse: async (arg) => {
+        return fetch(`${API_URL}/api/courses/update/${arg.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
                 "Accept": "application/json",
             },
             body: JSON.stringify(arg)
@@ -91,7 +154,65 @@ export const root = {
                 console.log(data)
                 return data
             })
-        
-    }
- 
+    },
+
+    updateHistory: async (arg) => {
+        return fetch(`${API_URL}/api/history/update/${arg.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(arg)
+        })
+            .then(response => response.json())
+            .then(data => {
+                return data
+            })
+    },
+
+    getCourseName: (arg) => {
+        let query = `
+        {
+            asignatura(codigo_asignatura: ${arg.id_asignature}) {
+                codigo_asignatura
+            }
+        }`
+
+        return fetch(`${API_URL_BUSCADOR_CURSOS}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                query,
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                return data
+            })
+    },
+
+    getStudents: (arg) => {
+        let query = `inscripcionByIdCurso(id_curso: ${arg.id_course}){
+            documento_estudiante
+        }`
+
+        return fetch(`${API_URL_INSCRIPCIONES}/inscripcion/${arg.id_course}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query,
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                return data
+            })
+    },
+
 }
--- sia_calificaciones/schema.js
+++ sia_calificaciones/schema.js
@@ -7,22 +7,49 @@ import { buildSchema } from "graphql"
 
 const schema = buildSchema(`
     type Query {
+<<<<<<< Updated upstream
         listGrades(id: Int, asignature: Int): [Grade]
         listAsignatures(id: Int, termn: Int): [Asignature]
         listHistory(input: Int, program: Int): [History]
+=======
+        listAll(id: Int): All
+        listByStudent(id: String, term: String): [StudentsGrade]
+        listGrades(id: Int, course: Int): [Grade]
+        listCourse(id: Int, termn: String): [Course]
+        listHistory(id: Int, program: Int): [History]
+        getCourseName(id_course: Int): String
+        getStudents(id_course: Int): 
+    }
+
+    type StudentsGrade {
+        id_student: String
+        id_course: Int
+        grades: String
+    }
+
+    type All {
+        history: [History]
+        courses: [Course]
+        grades: [Grade]
+>>>>>>> Stashed changes
     }
 
     type Grade {
         id: Int
-        id_asignature: Int
+        id_course: Int
         name: String
         percentage: Float
+<<<<<<< Updated upstream
         id_students: String
         values: String
+=======
+        grades: String
+>>>>>>> Stashed changes
     }
 
-    type Asignature {
+    type Course{
         id: Int
+        id_asignature: Int
         term: String
         consolidated: Boolean
         grades: String
@@ -33,24 +60,26 @@ const schema = buildSchema(`
         id_student: Int
         id_program: Int
         percentage_adv: Float
-        asignature_taken: String
-    }
-
-    input GradeInput {
-        id: Int
-        id_asignature: Int
-        name: String
-        percentage: Float
-        id_students: String
-        values: String
+        asignature_taken: [Int]
     }
     
+    type gradesT {
+        id_student: String
+        value: Float
+    }
+
+    input gradesInput {
+        id_student: String
+        value: Float
+    }
+
     type Mutation {
         createGrades(
             id: Int
-            id_asignature: Int
+            id_course: Int
             name: String
             percentage: Float
+<<<<<<< Updated upstream
             id_students: Array
             values: Array): Grade
         updateGrades(input: GradeInput): Message
@@ -59,6 +88,37 @@ const schema = buildSchema(`
 
     input Array {
         name: [Int]
+=======
+            grades: String
+            ): Grade
+            
+        updateGrades(
+            id: Int
+            id_course: Int
+            name: String
+            percentage: Float
+            grades: String
+            ): Grade
+            
+        deleteGrades(
+            input: Int
+            ): Message
+
+        updateCourse(
+            id: Int
+            id_asignature: Int
+            term: String
+            consolidated: Boolean
+            ): Course
+            
+        updateHistory(
+            id: Int
+            id_student: String
+            id_program: Int
+            percentage_adv: Float
+            asignature_taken: [Int]
+            ): History
+>>>>>>> Stashed changes
     }
 
     type Message {
