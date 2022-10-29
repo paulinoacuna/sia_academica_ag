544882de8d11a8ce7202327fe645b3902345609d
jorodriguezal <72422459+jorodriguezal@users.noreply.github.com>
2022-10-28 21:20:14 -0500

    Ajuste rutas

--- routes.js
+++ routes.js
@@ -9,11 +9,11 @@ const routes = {
     route: "/info_academica",
   },
   buscadorMaterias: {
-    url:  "http://localhost:4100/",//"http://host.docker.internal:4100", 
+    url: "http://http://host.docker.internal:4100/", //"http://host.docker.internal:4100",
     route: "/buscador_cursos",
   },
   inscripciones: {
-    url: "http://127.0.0.1:4000",//"http://host.docker.internal:4000",
+    url: "http://http://host.docker.internal:4000", //"http://host.docker.internal:4000",
     route: "/inscripciones",
   },
   calificaciones: {
