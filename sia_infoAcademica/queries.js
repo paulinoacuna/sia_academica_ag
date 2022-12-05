386c7a82c9de46e0a5bb74b5b30fbd6d1a3e4acf
paulinoacuna <paulinomaven911@gmail.com>
2022-12-04 22:24:55 -0500

    fix

--- sia_infoAcademica/queries.js
+++ sia_infoAcademica/queries.js
@@ -1,6 +1,6 @@
 export const getHistoryData = (args) => `
     {
-        listHistory(id: "${args}", program: null) {
+        listHistory(id_student: "${args}", program: null) {
             id
             id_student
             id_program
